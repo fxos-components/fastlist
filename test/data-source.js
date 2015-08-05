17e10e57c50401ebd19d37a1bae145bd410bced6
Etienne Segonzac <etienne@segonzac.info>
2015-08-05 15:50:22 +0200

    Coverig the rendering part with unit tests. Fixes issue #16

---
+++ test/data-source.js
@@ -0,0 +1,74 @@
+(function(exports) {
+  'use strict';
+
+  var itemHeight = 64;
+
+  exports.DataSource = function DataSource(items) {
+    this.items = items;
+  };
+
+  exports.DataSource.prototype = {
+    populateItem: function(item, i) {
+      var title = item.querySelector('h3');
+      var body = item.querySelector('p');
+      var record = this.getRecordAt(i);
+
+      title.textContent = record.title;
+      body.textContent = record.body;
+    },
+
+    getSections: function() {
+      return ['Section Name'];
+    },
+
+    sectionHeaderHeight: function() {
+      return 0;
+    },
+
+    fullSectionHeight: function() {
+      var result = this.items.length * itemHeight;
+      return result;
+    },
+
+    fullSectionLength: function() {
+      var result = this.items.length;
+      return result;
+    },
+
+    getSectionFor: function() {
+      return 'Section Name';
+    },
+
+    getRecordAt: function(index) {
+      return this.items[index];
+    },
+
+    indexAtPosition: function(pos) {
+      return Math.floor(pos / itemHeight);
+    },
+
+    positionForIndex: function(index) {
+      return index * itemHeight;
+    },
+
+    fullLength: function() {
+      return this.items.length;
+    },
+
+    itemHeight: function() {
+      return itemHeight;
+    },
+
+    fullHeight: function() {
+      this.items.length * itemHeight;
+    },
+
+    insertAtIndex: function(index, record, toSection) {
+      return this.items.splice(index, 0, record);
+    },
+
+    replaceAtIndex: function(index, record) {
+      return this.items.splice(index, 1)[0];
+    }
+  };
+})(window);
